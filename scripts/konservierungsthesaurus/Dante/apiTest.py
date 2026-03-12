5b30d55a56feff67355a751ffe955045fe06d6b5
Lasse Mempel-Länger <lassemempel@gmail.com>
2026-03-12 11:12:55 +0100

    starting export ttl synchronization

--- scripts/konservierungsthesaurus/Dante/apiTest.py
+++ scripts/konservierungsthesaurus/Dante/apiTest.py
@@ -1,61 +1,40 @@
 import requests
+import csv
 import json
 import os
 
-if not os.path.exists("scheme.json"):
-    schemeJSON = requests.get("https://api.dante.gbv.de/voc/leiza_archlink?properties=*")
-    with open("scheme.json", "w") as f:
-        json.dump(schemeJSON.json(), f, indent=2)
-with open("scheme.json", "r") as f:
-    schemeJSON = json.load(f)
+checksumFileLink = "https://api.dante.gbv.de/export/download/leiza_archlink/Standardexport/checksums.txt"
+vocabularyFileLink = "https://api.dante.gbv.de/export/download/leiza_archlink/Standardexport/leiza_archlink__Standardexport.turtle.ttl"
 
-if not os.path.exists("topConcepts.json"):
-    conceptsJSON = requests.get("https://api.dante.gbv.de/voc/leiza_archlink/top")
-    with open("topConcepts.json", "w") as f:
-        json.dump(conceptsJSON.json(), f, indent=2)
-with open("topConcepts.json", "r") as f:
-    conceptsJSON = json.load(f)
+def readChecksumFile(file):
+    with open(file, "r") as f:
+        checksumTable = list(csv.reader(f))
+        ttlRow = checksumTable[9][0]
+        ttlColumns = ttlRow.split()
+        if ttlColumns[0] == "leiza_archlink__Standardexport.turtle.ttl":
+            ttlChecksum = ttlColumns[2]
+            return ttlChecksum
+        else:
+            raise ValueError("Unexspected data in checksums.txt")
 
-def crawlConcept(uri, conceptArray, i):
-    print(f"  → Processing concept {i}: {uri}")
-    conceptTree = requests.get(f"https://api.dante.gbv.de/data?uri={uri}&properties=*")
-    concept = conceptTree.json()[0]
-    conceptArray.append(concept)
-    for narrower in (concept.get("narrower") or []):
-        conceptArray, i = crawlConcept(narrower["uri"], conceptArray, i)
-    return conceptArray, i+1
+originalChecksum = readChecksumFile("checksums.txt")
 
-# Crawl and save each top concept tree individually
+checkSumFile = requests.get(checksumFileLink)
+checkSumFileContent = checkSumFile.text
+with open("newChecksums.txt", "w") as file:
+    file.write(checkSumFileContent)
 
-for topConcept in conceptsJSON:
-    topConceptUri = topConcept["uri"]
-    print("/n")
-    print(f"\nProcessing top concept: {topConceptUri}")
-    concept_id = topConceptUri.split("/")[-1]
-    filename = f"{concept_id}.json"
+newChecksum = readChecksumFile("newChecksums.txt")
 
-    if not os.path.exists(filename):
-        print(f"Crawling tree of {topConceptUri}")
-        i = 1
-        conceptArray = crawlConcept(topConceptUri, [], i)
-        with open(filename, "w") as f:
-            json.dump(conceptArray, f, indent=2)
-        print(f"  → Saved {len(conceptArray)} concepts to {filename}")
-    else:
-        print(f"Skipping {filename} (already exists)")
+if newChecksum == originalChecksum:
+    print("No changes in vocabulary file")
+    os.remove("newChecksums.txt")
+else:
+    print("Vocabulary file has changed")
+    print("Replacing checksum.txt with newChecksum.txt")
+    os.replace("newChecksums.txt", "checksums.txt")
+    
 
-# Combine all individual files into concepts.json
-if not os.path.exists("concepts.json"):
-    allConcepts = []
-    for topConcept in conceptsJSON:
-        concept_id = topConcept["uri"].split("/")[-1]
-        filename = f"{concept_id}.json"
-        with open(filename, "r") as f:
-            allConcepts.extend(json.load(f))
-    with open("concepts.json", "w") as f:
-        json.dump(allConcepts, f, indent=2)
-    print(f"\nCombined {len(allConcepts)} total concepts into concepts.json")
+#vocabularyFile = requests.get(vocabularyFileLink)
+#vocabularyFileContent = vocabularyFile.text
 
-with open("concepts.json", "r") as f:
-    conceptsJSON = json.load(f)
-print(json.dumps(conceptsJSON, indent=2))
